4a06a4e7b76c4e9c3fc147c7d3d7637ea4265902
bibekkhatiwada7 <bibekhatiwada550@gmail.com>
2025-03-30 23:04:37 -0400

    added basic office material

--- js/vr-office.js
+++ js/vr-office.js
@@ -13,11 +13,36 @@ window.addEventListener('DOMContentLoaded', async function(){
     light.intensity = 0.8;
 
     // Added ground to the scene
-     
     const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 15, height: 15 }, scene);
     const groundMat = new BABYLON.StandardMaterial("groundMat", scene);
     groundMat.diffuseColor = new BABYLON.Color3(0.85, 0.85, 0.85); // light gray
     ground.material = groundMat;
+
+
+    // Added a computer
+    const computer = BABYLON.MeshBuilder.CreateBox("computer", { width: 0.8, height: 0.5, depth: 0.1 }, scene);
+    computer.position.y = 1.25;
+    computer.position.z = -3;
+    const computerMat = new BABYLON.StandardMaterial("computerMat", scene);
+    computerMat.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+    computer.material = computerMat;
+
+    // Added a desk
+    const desk = BABYLON.MeshBuilder.CreateBox("desk", { width: 2.4, height: 1, depth: 1 }, scene);
+    desk.position.y = 0.5;
+    desk.position.z = -3.2;
+    const deskMat = new BABYLON.StandardMaterial("deskMat", scene);
+    deskMat.diffuseColor = new BABYLON.Color3(0.58, 0.58, 0.88); 
+    desk.material = deskMat;
+
+   // Added a chair
+   const chair = BABYLON.MeshBuilder.CreateCylinder("chair", { diameter: 0.8, height: 0.5 }, scene);
+   chair.position.y = 0.25;
+   chair.position.z = -1.5;
+   const chairMat = new BABYLON.StandardMaterial("chairMat", scene);
+   chairMat.diffuseColor = new BABYLON.Color3(0.6, 0.4, 0.3);
+   chair.material = chairMat;
+
 
     // Start a WebXR session 
     const xr = await scene.createDefaultXRExperienceAsync({
