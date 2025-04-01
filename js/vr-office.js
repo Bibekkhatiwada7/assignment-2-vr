f1d8e12e2910c96aef38efbcf07420be63e57749
bibekkhatiwada7 <bibekhatiwada550@gmail.com>
2025-04-01 14:43:18 -0400

    background color

--- js/vr-office.js
+++ js/vr-office.js
@@ -1,5 +1,5 @@
-// Basic scene setup
-window.addEventListener('DOMContentLoaded', async function(){
+    // Basic scene setup
+    window.addEventListener('DOMContentLoaded', async function(){
     const canvas = document.getElementById('renderCanvas');
     const engine = new BABYLON.Engine(canvas, true);
     const scene = new BABYLON.Scene(engine);
@@ -11,14 +11,14 @@ window.addEventListener('DOMContentLoaded', async function(){
     // Added the Light
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.8;
-
+    scene.clearColor = new BABYLON.Color3(0.8, 0.9, 1);//background colour 
+   
     // Added ground to the scene
     const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 15, height: 15 }, scene);
     const groundMat = new BABYLON.StandardMaterial("groundMat", scene);
     groundMat.diffuseColor = new BABYLON.Color3(0.85, 0.85, 0.85); // light gray
     ground.material = groundMat;
-
-
+    
     // Added a computer
     const computer = BABYLON.MeshBuilder.CreateBox("computer", { width: 0.8, height: 0.5, depth: 0.1 }, scene);
     computer.position.y = 1.25;
